fae0bc8291f73d3a68d7c363844c2dde2f1d4871
Souleimane-ifqirne <ifqirne.souleimane@gmail.com>
2023-09-07 17:13:26 +0200

    feat(job1): mychangebackgroundcolor in js

---
+++ jour-02/job-01/script.js
@@ -0,0 +1,15 @@
+function myChangeBackgroundColor() {
+    var screenSize = window.innerWidth
+
+    if (screenSize <= 500) {
+        document.body.style.backgroundColor="#003049"
+    } else if (screenSize <= 1336) {
+        document.body.style.backgroundColor="#d90429"
+    } else {
+        document.body.style.backgroundColor="#ffb703"
+    }
+}
+
+window.addEventListener("resize", myChangeBackgroundColor);
+
+myChangeBackgroundColor();
